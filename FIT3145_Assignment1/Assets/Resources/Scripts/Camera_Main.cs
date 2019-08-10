fe94328c78227ade646baf970aedcf29b8a64cc0
mgri0003 <mgri0003@student.monash.edu>
2019-08-10 16:11:23 +1000

    Movement & Camera Implementation
    - 3rd person camera
    - camera based movement
    - small memery

---
+++ FIT3145_Assignment1/Assets/Resources/Scripts/Camera_Main.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Main : MonoBehaviour
+{
+    //variables
+    [SerializeField] private Transform m_target = null;
+    [SerializeField] private Transform m_cameraHolder = null;
+    private float m_currentRotationX;
+    private float m_currentRotationY;
+
+
+    //methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //error check references!
+        Debug.Assert(m_target != null, "Target Is Null");
+        Debug.Assert(m_cameraHolder != null, "Camera Holder Is Null");
+
+        //hide da mouse
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Camera Position
+        m_cameraHolder.SetPositionAndRotation(m_target.position, m_cameraHolder.rotation);
+
+        //Camera Rotation
+        m_currentRotationX = m_target.rotation.eulerAngles.y;
+        m_currentRotationY -= Input.GetAxis("Mouse Y");
+        m_currentRotationY = Mathf.Clamp(m_currentRotationY, -15, 60);
+        m_cameraHolder.transform.rotation = Quaternion.Euler(m_currentRotationY, m_currentRotationX, 0);
+    }
+}
---
+++ FIT3145_Assignment1/Assets/Resources/Scripts/Player_Core.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Core : MonoBehaviour
+{
+    //Variables
+    [SerializeField] private float m_movementSpeed = 0.05f;
+    float m_currentRotation = 0;
+
+    //Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationX_Delta = Input.GetAxis("Mouse X");
+        float hVal = Input.GetAxis("Horizontal");
+        float vVal = Input.GetAxis("Vertical");
+
+        if(hVal != 0 || vVal != 0)
+        {
+            transform.Translate(new Vector3(hVal, 0, vVal) * m_movementSpeed, Space.Self);
+        }
+
+        m_currentRotation += rotationX_Delta;
+        transform.rotation = Quaternion.Euler(0, m_currentRotation, 0);
+
+    }
+}
