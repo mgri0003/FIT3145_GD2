aee3343e2605866201a0a5971296006b0987de81
mgri0003 <mgri0003@student.monash.edu>
2019-08-10 18:25:45 +1000

    Models,Animations & Project Structure
    - Added newly made models
    - Added in some custom animations (Idle & Walk)
    - Foundations of animation system for player
    - structure of project assets

--- FIT3145_Assignment1/Assets/Resources/Scripts/Player_Core.cs
+++ FIT3145_Assignment1/Assets/Resources/Scripts/Player_Core.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player_Core : MonoBehaviour
 {
     //Variables
+    private Animator m_animator;
     [SerializeField] private float m_movementSpeed = 0.05f;
     float m_currentRotation = 0;
 
@@ -13,23 +14,33 @@ public class Player_Core : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float rotationX_Delta = Input.GetAxis("Mouse X");
+        UpdateMovement();
+        UpdateRotation();
+    }
+
+    void UpdateMovement()
+    {
         float hVal = Input.GetAxis("Horizontal");
         float vVal = Input.GetAxis("Vertical");
+        bool isMoving = (hVal != 0 || vVal != 0);
 
-        if(hVal != 0 || vVal != 0)
+        if (isMoving)
         {
             transform.Translate(new Vector3(hVal, 0, vVal) * m_movementSpeed, Space.Self);
         }
 
-        m_currentRotation += rotationX_Delta;
-        transform.rotation = Quaternion.Euler(0, m_currentRotation, 0);
+        m_animator.SetBool("AP_isMoving", isMoving);
+    }
 
+    void UpdateRotation()
+    {
+        m_currentRotation += Input.GetAxis("Mouse X");
+        transform.rotation = Quaternion.Euler(0, m_currentRotation, 0);
     }
 }
