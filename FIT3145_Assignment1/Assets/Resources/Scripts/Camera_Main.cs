f141f4f70de1c8ce2d989b4a983fad5b716bd8fc
mgri0003 <mgri0003@student.monash.edu>
2019-08-12 15:41:12 +1000

    Camera Modes
    - Camera now has two modes (Right & Left)
    - This mode can be toggled with Middle Mouse Click

--- FIT3145_Assignment1/Assets/Resources/Scripts/Camera_Main.cs
+++ FIT3145_Assignment1/Assets/Resources/Scripts/Camera_Main.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class Camera_Main : MonoBehaviour
 {
-    //variables
+    //--pre-declared variables--
+    enum ECamera_ViewMode
+    {
+        LEFT_SIDE,
+        RIGHT_SIDE,
+        MAX
+    };
+
+
+    //--variables--
     [SerializeField] private Transform m_target = null;
     [SerializeField] private Transform m_cameraHolder = null;
     private float m_currentRotationX;
     private float m_currentRotationY;
+    const float m_currentZoom = 2.5f;
+    const float m_currentHeight = 1.5f;
+    private ECamera_ViewMode m_cameraViewMode = ECamera_ViewMode.RIGHT_SIDE;
+
+    readonly Vector3 CameraView_LeftSide    = new Vector3(-1 , m_currentHeight, -m_currentZoom);
+    readonly Vector3 CameraView_RightSide   = new Vector3(1  , m_currentHeight, -m_currentZoom);
 
 
-    //methods
+    //--methods--
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +40,8 @@ public class Camera_Main : MonoBehaviour
         //hide da mouse
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        RefreshCameraViewMode();
     }
 
     // Update is called once per frame
@@ -36,5 +55,43 @@ public class Camera_Main : MonoBehaviour
         m_currentRotationY -= Input.GetAxis("Mouse Y");
         m_currentRotationY = Mathf.Clamp(m_currentRotationY, -15, 60);
         m_cameraHolder.transform.rotation = Quaternion.Euler(m_currentRotationY, m_currentRotationX, 0);
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            CycleCameraViewMode();
+        }
+    }
+
+
+    void CycleCameraViewMode()
+    {
+        if((int)m_cameraViewMode < (int)ECamera_ViewMode.MAX - 1)
+        {
+            m_cameraViewMode++;
+        }
+        else
+        {
+            m_cameraViewMode = (ECamera_ViewMode)(0);
+        }
+
+        RefreshCameraViewMode();
+    }
+
+    void RefreshCameraViewMode()
+    {
+        switch (m_cameraViewMode)
+        {
+            case ECamera_ViewMode.LEFT_SIDE:
+            {
+                transform.localPosition = CameraView_LeftSide;
+            };
+            break;
+
+            case ECamera_ViewMode.RIGHT_SIDE:
+            {
+                transform.localPosition = CameraView_RightSide;
+            };
+            break;
+        }
     }
 }
