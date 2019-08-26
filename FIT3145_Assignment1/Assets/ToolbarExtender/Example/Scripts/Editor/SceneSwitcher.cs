42db5569f2d0f2bcad9658663c3bf1dcc095bcca
mgri0003 <mgri0003@student.monash.edu>
2019-08-26 14:57:18 +1000

    UI EXTENSIONS!!!

---
+++ FIT3145_Assignment1/Assets/ToolbarExtender/Example/Scripts/Editor/SceneSwitcher.cs
@@ -0,0 +1,78 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace UnityToolbarExtender.Examples
+{
+	static class ToolbarStyles
+	{
+		public static readonly GUIStyle commandButtonStyle;
+
+		static ToolbarStyles()
+		{
+            commandButtonStyle = new GUIStyle("Command")
+            {
+                fontSize = 12,
+                alignment = TextAnchor.MiddleCenter,
+                imagePosition = ImagePosition.ImageAbove,
+                fontStyle = FontStyle.Bold,
+                fixedWidth = 160,
+                fixedHeight = 22
+            };
+		}
+	}
+
+	[InitializeOnLoad]
+	public class SceneSwitchLeftButton
+	{
+		static SceneSwitchLeftButton()
+		{
+			ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+		}
+
+		static void OnToolbarGUI()
+		{
+            GUILayout.Space(50);
+
+            if (GUILayout.Button(new GUIContent("Play From Main Menu", "Play From Main Menu"), ToolbarStyles.commandButtonStyle))
+			{
+				SceneHelper.StartScene("Assets/Scenes/MainMenu.unity");
+			}
+        }
+	}
+
+	static class SceneHelper
+	{
+		static string sceneToOpen;
+
+		public static void StartScene(string scene)
+		{
+			if(EditorApplication.isPlaying)
+			{
+				EditorApplication.isPlaying = false;
+			}
+
+			sceneToOpen = scene;
+			EditorApplication.update += OnUpdate;
+		}
+
+		static void OnUpdate()
+		{
+			if (sceneToOpen == null ||
+			    EditorApplication.isPlaying || EditorApplication.isPaused ||
+			    EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode)
+			{
+				return;
+			}
+
+			EditorApplication.update -= OnUpdate;
+
+			if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+			{
+				EditorSceneManager.OpenScene(sceneToOpen);
+				EditorApplication.isPlaying = true;
+			}
+			sceneToOpen = null;
+		}
+	}
+}
