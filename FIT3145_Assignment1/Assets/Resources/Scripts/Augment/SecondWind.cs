e0fbc86f5508704f79c6ccd529647ec533379bcf
mgri0003 <mgri0003@student.monash.edu>
2019-09-22 15:57:59 +1000

    Added new augment - Second Wind
    new augment that revives the player when death normally occurs, has a 5 minute cooldown

---
+++ FIT3145_Assignment1/Assets/Resources/Scripts/Augment/SecondWind.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondWind : Augment
+{
+    protected override bool AugmentAbility()
+    {
+        if(m_player.IsDead())
+        {
+            m_player.Revive();
+            m_player.m_characterStats.AccessHealthStat().SetCurrent(m_player.m_characterStats.AccessHealthStat().GetMax() / 2);
+            FX_Manager.Instance.SpawnParticleEffect(EParticleEffect.SECOND_WIND, m_player.transform.position);
+            return true;
+        }
+
+        return false;
+    }
+
+    public override void AugmentOnDeath()
+    {
+        ActivateAugmentAbility();
+    }
+
+    protected override void AugmentUpdate()
+    {
+
+    }
+}
