5de609a2d0c99cab3cac00d8e58dcafc7e1f94c6
mgri0003 <mgri0003@student.monash.edu>
2019-09-22 14:21:50 +1000

    New Upgrade - Regenerating Magazine
    - imported new model for regenerating magazine
    - new upgrade that regenerated ammo every few seconds.

---
+++ FIT3145_Assignment1/Assets/Resources/Scripts/Upgrade/RegeneratingMagazine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegeneratingMagazine : Upgrade
+{
+    private const int BULLET_REGEN = 1;
+    private const int REGEN_TIME_INTERVAL = 2;
+    private float m_regenTime = REGEN_TIME_INTERVAL;
+
+    public override string GetItemTypeDescription()
+    {
+        return "Regenerates " + BULLET_REGEN + " ammo every " + REGEN_TIME_INTERVAL + " seconds";
+    }
+
+    protected override void ApplyUpgradeSettings()
+    {
+        
+    }
+
+    protected override void OnUpgradeUpdate()
+    {
+        if(m_regenTime > 0.0f)
+        {
+            m_regenTime -= Time.deltaTime;
+        }
+
+        if(m_regenTime <= 0.0f)
+        {
+            m_regenTime = REGEN_TIME_INTERVAL;
+
+            RegenAmmo();
+        }
+    }
+
+    protected override void ResetUpgradeSettings()
+    {
+        
+    }
+
+    private void RegenAmmo()
+    {
+        (GetParentWeapon() as Weapon_Ranged).AddCurrentAmmo(1);
+    }
+
+}
