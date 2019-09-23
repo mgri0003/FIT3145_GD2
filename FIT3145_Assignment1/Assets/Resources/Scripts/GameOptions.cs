4887525581e4613bc2aa03bf50a8160e24f77e70
mgri0003 <mgri0003@student.monash.edu>
2019-09-23 16:22:10 +1000

    Quality Settings Option Implemented
    - new option in option menu, can now change quality level dynamically

--- FIT3145_Assignment1/Assets/Resources/Scripts/GameOptions.cs
+++ FIT3145_Assignment1/Assets/Resources/Scripts/GameOptions.cs
@@ -6,12 +6,20 @@ public static class GameOptions
 {
     //--Variables--//
     private static bool m_swapRightLeftClick = false;
+    private static int m_qualitySetting = QualitySettings.GetQualityLevel();
 
     //--Getters--//
     public static bool GetSwapRightLeftClick() { return m_swapRightLeftClick; }
+    public static int GetQualitySetting() { return m_qualitySetting; }
+    public static string GetQualitySettingName() { return QualitySettings.names[m_qualitySetting]; }
 
     //--Setters--//
     public static void SetSwapRightLeftClick(bool newValue) { m_swapRightLeftClick = newValue; }
+    public static void SetQualitySetting(int newValue)
+    {
+        m_qualitySetting = newValue;
+        QualitySettings.SetQualityLevel(GetQualitySetting());
+    }
 
     //--Toggles--//
     public static void ToggleSwapRightLeftClick() { m_swapRightLeftClick = !m_swapRightLeftClick; }
--- FIT3145_Assignment1/Assets/Resources/Scripts/UI/UIScreen_GameOptions.cs
+++ FIT3145_Assignment1/Assets/Resources/Scripts/UI/UIScreen_GameOptions.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 enum EGameOption
 {
     TOGGLE_SWAPLEFTRIGHTCLICK,
+    DROPDOWN_QUALITYSETTING,
 
     MAX
 }
@@ -17,6 +18,7 @@ public class UIScreen_GameOptions : UIScreenBase
 {
     //--UI Elements--//
     [SerializeField] private Toggle m_toggle_SwapLeftRightClick;
+    [SerializeField] private Dropdown m_dropdown_qualitySettings;
 
     [SerializeField] private Button m_backButton;
 
@@ -24,7 +26,8 @@ public class UIScreen_GameOptions : UIScreenBase
     {
         m_backButton.onClick.AddListener(() => { OnBack(); });
 
-        m_toggle_SwapLeftRightClick.onValueChanged.AddListener((bool val) => { ChangeOption(EGameOption.TOGGLE_SWAPLEFTRIGHTCLICK); });
+        m_toggle_SwapLeftRightClick.onValueChanged.AddListener((bool val) => { OnOptionChanged(EGameOption.TOGGLE_SWAPLEFTRIGHTCLICK); });
+        m_dropdown_qualitySettings.onValueChanged.AddListener((int val) => { OnOptionChanged(EGameOption.DROPDOWN_QUALITYSETTING); });
     }
 
     protected override void OnDisable()
@@ -34,7 +37,9 @@ public class UIScreen_GameOptions : UIScreenBase
 
     protected override void OnEnable()
     {
-        
+        PopulateQualitySettingsDropDown();
+
+        RefreshOptionsUI();
     }
 
     protected override void OnGUI()
@@ -50,18 +55,44 @@ public class UIScreen_GameOptions : UIScreenBase
     private void RefreshOptionsUI()
     {
         m_toggle_SwapLeftRightClick.isOn = GameOptions.GetSwapRightLeftClick();
+        m_dropdown_qualitySettings.value = GameOptions.GetQualitySetting();
     }
 
-    private void ChangeOption(EGameOption gameOption)
+    private void OnOptionChanged(EGameOption gameOption)
     {
         switch (gameOption)
         {
             case EGameOption.TOGGLE_SWAPLEFTRIGHTCLICK:
             {
                 GameOptions.SetSwapRightLeftClick(m_toggle_SwapLeftRightClick.isOn);
-                Debug.Log("Option Changed to : " + GameOptions.GetSwapRightLeftClick());
+                //Debug.Log("Option Changed to : " + GameOptions.GetSwapRightLeftClick());
+            }
+            break;
+
+            case EGameOption.DROPDOWN_QUALITYSETTING:
+            {
+                GameOptions.SetQualitySetting(m_dropdown_qualitySettings.value);
+                //Debug.Log("Option Changed to : " + GameOptions.GetQualitySettingName() + "(" + GameOptions.GetQualitySetting() + ")"); 
             }
             break;
         }
+
+        RefreshOptionsUI();
+    }
+
+    private void PopulateQualitySettingsDropDown()
+    {
+        m_dropdown_qualitySettings.ClearOptions();
+        List<Dropdown.OptionData> newOptions = new List<Dropdown.OptionData>();
+        foreach (string qualityOptionName in QualitySettings.names)
+        {
+            Dropdown.OptionData newOption = new Dropdown.OptionData
+            {
+                text = qualityOptionName
+            };
+
+            newOptions.Add(newOption);
+        }
+        m_dropdown_qualitySettings.AddOptions(newOptions);
     }
 }
